4dc36d4239351c2132b469cb975d5afe12743786
Miral Kumbhani <kumbhanimiral@gmail.com>
2024-05-02 15:44:57 +0530

    Updated the code with the functional approach

--- app.js
+++ app.js
@@ -1,44 +1,12 @@
-const { sum } = require("./helper");
-const http = require('http');
-const express = require('express');
 const fs = require('fs');
 
-console.log("Hello from Node.js!")
-// console.log('Process: ', process);
+const fileName = "target.txt";
 
-// Simple function for summing two numbers
-const total = sum(5, 6);
-console.log('Total from the function sum is:', total);
+const errorHandler = err => console.error(err);
 
-// Creating a server
-const server = http.createServer((req, res) => {
-    res.end('Hello World! This is the response from the server.');
-});
+const dataHandler = data => console.log('Data from file: ', data.toString());
 
-server.listen(3000);
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send("Hey! This is the first API from express!");
-})
-
-app.listen(3001);
-
-const fileName= "target.txt";
-
-// fs.watch(fileName, () => console.log('File System!'));
-
-// asynchronous call
 fs.readFile(fileName, (err, data) => {
-    if (err) {
-        console.error(err);
-    }
-    console.log('Data of file: ', data.toString());
+    if (err) errorHandler(err)
+    dataHandler(data);
 });
-
-// synchronous call
-// const data = fs.readFileSync(fileName);
-// console.log('Data: ', data.toString())
-
-console.log('async call!')
